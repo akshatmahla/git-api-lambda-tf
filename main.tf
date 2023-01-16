2559afc2b7f070879bb1d19fbd4b946bb5702b79
akshat mahla <akshat.mahla@avon.com>
2023-01-16 21:27:10 +0530

    added lambda

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "../lambdaSendEnquiry/lambda.zip"
+  filename      = "lambda.zip"
   function_name = var.lambda_function_name
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
